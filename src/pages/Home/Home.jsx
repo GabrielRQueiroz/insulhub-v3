ad794be2e6b4566a3f413d5ceda06321c28d9832
Gabriel Queiroz <gabrielr.queiroz@gmail.com>
2022-02-11 01:31:31 -0300

    v0.3.4 - Quick design update and minor changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,10 @@
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import React, { forwardRef, useState } from 'react';
-import DatePicker from 'react-datepicker';
+import React, { useState } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import { FaCalculator, FaCarrot, FaPencilAlt, FaRegCalendarAlt, FaRegClipboard, FaRegClock } from 'react-icons/fa';
 import { Graph, PageHeader, Readings } from '../../components';
 import {
-	DatePickerButton,
 	DateText,
 	DateWrapper,
 	GraphContainer,
@@ -19,22 +17,14 @@ import {
 	HomeUsernameField,
 	MainSectionContainer,
 	ReadingsWrapper,
-	SearchWrapper,
-	StyledTimePicker,
-	TimeSectionContainer,
-	TimeSectionWrapper,
+	SearchWrapper, StyledDatePicker, StyledTimePicker,
+	TimeSectionWrapper
 } from './HomeElements';
 
 export const Home = () => {
 	const [date, setDate] = useState(new Date());
 	const [time, setTime] = useState(date);
 	const [username, setUsername] = useState(localStorage.username || '');
-
-	const ButtonRef = forwardRef(({ value, onClick }, ref) => (
-		<DatePickerButton onClick={onClick} ref={ref}>
-			{value}
-		</DatePickerButton>
-	));
 
 	const handleDateChange = (date) => {
 		setDate(date);
@@ -48,60 +38,54 @@ export const Home = () => {
 	};
 
 	return (
-		<HomeContainer>
-			<PageHeader heading='Início'>
-				<HomeGreetings>
-					Olá, <HomeUsernameField onSave={(e) => saveUsername(e.value)} placeholder='usuário' defaultValue={username} />
-					{'! '}
-					<FaPencilAlt />
-				</HomeGreetings>
-			</PageHeader>
-			<HomeCardsContainer>
-				<HomeCard to='/summary'>
-					<HomeCardTitle>Relatório</HomeCardTitle>
-					<FaRegClipboard />
-				</HomeCard>
-				<HomeCard to='/calculator'>
-					<HomeCardTitle>Regra de Três</HomeCardTitle>
-					<FaCalculator />
-				</HomeCard>
-				<HomeCard to='/food'>
-					<HomeCardTitle>Alimentos</HomeCardTitle>
-					<FaCarrot />
-				</HomeCard>
-			</HomeCardsContainer>
-			<MainSectionContainer>
-				<GraphSectionWrapper>
-					<DateWrapper>
-						<DateText>
-							<FaRegCalendarAlt />
-						</DateText>
-						<DatePicker
-							selected={date}
-							dateFormat='dd/MM/yyyy'
-							customInput={<ButtonRef />}
-							onChange={handleDateChange}
-							filterDate={(date) => new Date() > date} /* Excludes future date from the picker */
-						/>
-					</DateWrapper>
-					<GraphContainer>
-						<Graph selectedDate={date} />
-					</GraphContainer>
-				</GraphSectionWrapper>
-			</MainSectionContainer>
-			<TimeSectionContainer>
-				<TimeSectionWrapper>
-					<SearchWrapper>
-						<FaRegClock />
-						<MuiPickersUtilsProvider utils={DateFnsUtils}>
-							<StyledTimePicker inputVariant='outlined' ampm={false} value={time} onChange={handleTimeChange} />
-						</MuiPickersUtilsProvider>
-					</SearchWrapper>
-					<ReadingsWrapper>
-						<Readings selectedDate={date} selectedTime={time} />
-					</ReadingsWrapper>
-				</TimeSectionWrapper>
-			</TimeSectionContainer>
-		</HomeContainer>
+		<MuiPickersUtilsProvider utils={DateFnsUtils}>
+			<HomeContainer>
+				<PageHeader heading='Início'>
+					<HomeGreetings>
+						Olá, <HomeUsernameField onSave={(e) => saveUsername(e.value)} placeholder='usuário' defaultValue={username} />
+						{'! '}
+						<FaPencilAlt />
+					</HomeGreetings>
+				</PageHeader>
+				<HomeCardsContainer>
+					<HomeCard to='/summary'>
+						<HomeCardTitle>Relatório</HomeCardTitle>
+						<FaRegClipboard />
+					</HomeCard>
+					<HomeCard to='/calculator'>
+						<HomeCardTitle>Regra de Três</HomeCardTitle>
+						<FaCalculator />
+					</HomeCard>
+					<HomeCard to='/food'>
+						<HomeCardTitle>Alimentos</HomeCardTitle>
+						<FaCarrot />
+					</HomeCard>
+				</HomeCardsContainer>
+				<MainSectionContainer>
+					<GraphSectionWrapper>
+						<DateWrapper>
+							<DateText>
+								<FaRegCalendarAlt />
+							</DateText>
+							<StyledDatePicker inputVariant='outlined' disableFuture value={date} onChange={handleDateChange} autoOk />
+						</DateWrapper>
+						<GraphContainer>
+							<Graph selectedDate={date} />
+						</GraphContainer>
+						{/* <TimeSectionContainer> */}
+						<TimeSectionWrapper>
+							<SearchWrapper>
+								<FaRegClock />
+								<StyledTimePicker inputVariant='outlined' ampm={false} value={time} onChange={handleTimeChange} autoOk />
+							</SearchWrapper>
+							<ReadingsWrapper>
+								<Readings selectedTime={time} />
+							</ReadingsWrapper>
+						</TimeSectionWrapper>
+						{/* </TimeSectionContainer> */}
+					</GraphSectionWrapper>
+				</MainSectionContainer>
+			</HomeContainer>
+		</MuiPickersUtilsProvider>
 	);
 };
